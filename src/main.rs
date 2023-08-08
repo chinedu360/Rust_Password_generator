5b4a3c17576e7a8648acf37a9e82c4e29b0160ec
okere chinedu victor <okere360@gmail.com>
2023-08-08 23:35:02 +0100

    fix minor bugs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 use rand::{seq::SliceRandom, Rng};
 use std::io::stdin;
 
+const DIGITS: [char; 10] =[
+    '0', '1', '2', '3', '4', '5','6', '7', '8','9'
+];
+
 const ALPHABETS: [char; 26] = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z',
@@ -14,6 +18,17 @@ fn main() {
     create_password(length_of_password, name, val_digit)
 }
 
+fn generate_alhabet() -> String {
+    let mut random_letter = ALPHABETS.choose(&mut rand::thread_rng()).unwrap().to_string();
+    let to_uppercase = rand::thread_rng().gen::<bool>(); //get a random value which could be true or false
+
+    if to_uppercase {
+        random_letter = random_letter.to_uppercase();
+    }
+
+    random_letter
+}
+
 fn create_password(length_of_password: u32, name: String, val_digit: u32) {
 
     if val_digit > length_of_password {
@@ -22,22 +37,46 @@ fn create_password(length_of_password: u32, name: String, val_digit: u32) {
     }
 
     let mut vals: Vec<String> = Vec::new();
+    let mut random_digit: Vec<String> = Vec::new();
+    let mut random_alphabet: Vec<String> = Vec::new();
 
-    //creating a for loop to help achive the password length specified by the username
-    for i in 1..=length_of_password {
-
-        let random_letter = ALPHABETS.choose(&mut rand::thread_rng()).unwrap().to_string();
-        let to_uppercase = rand::thread_rng().gen::<bool>(); //we are just using this line to randomly get a boolen value.
-
-        if to_uppercase {
-            vals.push(random_letter.to_uppercase());
-        } else {
-            vals.push(random_letter);
-        }
+    for _i in 1..=val_digit { //add a new digit to random_digit for the length_of_password times
+        random_digit.push(DIGITS.choose(&mut rand::thread_rng()).unwrap().to_string()); //push a random digit into the vector until i is val_digits long
     }
 
-    //Note the vals is still a vector, to convert it to a string we have to call the join method on the vector and pass the empty string as a separator.
+    for _i in 1..=(length_of_password - val_digit) {//add a new alphabet to random_aplhabets for  length_of_password - val_of_digit number of times
+        random_alphabet.push(generate_alhabet())
+    }
 
+    //creating a for loop to help achive the password length specified by the username
+    // for i in 1..=length_of_password {
+
+    //     vals.push(generate_alhabet);//push a random alphabet into the vector of vals
+    // }
+
+    for _i in 1..length_of_password {
+        let chioce = match rand::thread_rng().gen_range(1..=2){
+            1 => {
+                if let Some(d) = random_digit.pop() {
+                    d
+                } else {
+                    random_alphabet.pop().unwrap()
+                }
+            },
+            2 => {
+                if let Some(a) = random_alphabet.pop() {    
+                    a
+                } else {
+                    random_digit.pop().unwrap()
+                }
+            },
+            _=> {String::from("This should not occur")}
+        };
+        vals.push(chioce);
+    }
+
+
+    //Note the vals is still a vector, to convert it to a string we have to call the join method on the vector and pass the empty string as a separator.
     let random_password: String = vals.join("");
     println!("{} This is your password: {}", name, random_password);
 }
@@ -60,9 +99,10 @@ fn get_password_length() -> u32 {
             _ => println!("The length is either invalid or must be a number"),
         }
     };
-    return pass_length;
+    pass_length
 }
 
+//posible improvement is to make the user input the number of digits they want.
 fn number_of_digits() -> u32 {
     let digit_num = loop {
         println!("Enter the number of digits you want in your password");
